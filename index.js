3a62702db8106ceeea0161d215efcfde499ed765
gorgeous-skill-0664 <wbugenis@gmail.com>
2020-10-02 03:41:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -56,7 +56,7 @@ function removeFromCart(item) {
     }
     i+= 1;
   }
-  return "That item is not in yuor cart.";
+  return "That item is not in your cart.";
 }
 
 function placeOrder(cardNumber) {
