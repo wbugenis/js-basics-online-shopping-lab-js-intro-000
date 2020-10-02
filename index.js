0c250389dfc54e931444dda0878c0da2ae5624da
gorgeous-skill-0664 <wbugenis@gmail.com>
2020-10-02 03:42:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -63,6 +63,8 @@ function placeOrder(cardNumber) {
   if(cardNumber === undefined) {
     return "Sorry, we don't have a credit card on file for you."
   } else {
-      return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`;
+      var final = `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`;
+      cart = [];
+      return final;
   }
 }
